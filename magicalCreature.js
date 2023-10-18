21ee0f7482e3d2e8d3dfbf42afcdbf2640484f2a
risktakerz <132370068+risktakerz@users.noreply.github.com>
2023-10-18 22:21:18 +0000

    I had created variables and assigned them to different strings and booleans, then you stopped at CanVisit variable

--- magicalCreature.js
+++ magicalCreature.js
@@ -1,25 +1,28 @@
 // Welcome to the magical zoo! Your task is to create and manage a list of magical creatures. Let's have some fun!
 
 // 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
-
+let creatureName = "Giant Toad";
 
 
 // 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
-
+let creatureDescription = "Super Jump."
 
 // 3. Do you believe that this creature is friendly to humans? 
 // Declare a variable named 'isFriendly' and assign it a boolean value.
-
+let isFriendly = false;
 
 
 // 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 1 and 60.
 // Hint: Use Math.floor() to round a number up.
 // Hint: Use Math.random() for generating random numbers.
 
-
+let vistitorAge = Math.floor(Math.random() * 60) + 1;
 
 // 5. Determine if the visitor is allowed to see the creature.
 // Declare a variable named 'canVisit' and assign it an expression to see 
+let canVisiit = vistitorAge >= creatureAge;
+
+
 // if the visitorAge variable is greater than 15.
 // Hint: use this resource if you need support: https://www.w3schools.com/js/js_booleans.asp 
 
